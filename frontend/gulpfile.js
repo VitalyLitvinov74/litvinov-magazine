f8ee270788a2451fd554021f43ca2d09986fd5d1
VitalyLitvinov74 <cheltend201@yandex.ru>
2022-02-15 23:07:59 +0500

    доработал генерацию фронтенд файлов

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+const { series } = require('gulp');
 
-exports.js = function (cb){
+function js (cb){
     return gulp.src([
         './node_modules/vue/dist/vue.js',
         './node_modules/http-vue-loader/src/httpVueLoader.js',
@@ -11,4 +12,6 @@ exports.js = function (cb){
         // .pipe(uglify())
         .pipe(concat('bundle.js'))
         .pipe(gulp.dest('./js'))
-};
+}
+
+exports.build = series(js);
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,5 +1,4 @@
 'use strict';
-
 var app = new Vue({
     el: '#app',
     data: {
