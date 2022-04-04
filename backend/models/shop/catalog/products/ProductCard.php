f7ee1c488b6716dbeb0e66d3e7b3fcecc99b7e3e
VitalyLitvinov74 <cheltend201@yandex.ru>
2022-04-05 01:24:23 +0500

    Update ProductCard.php

--- backend/models/shop/catalog/products/ProductCard.php
+++ backend/models/shop/catalog/products/ProductCard.php
@@ -5,8 +5,47 @@ namespace app\models\shop\catalog\products;
 
 
 use app\models\shop\catalog\products\contracts\IProductCard;
+use app\tables\TableProductCards;
+use vloop\entities\contracts\IField;
+use yii\db\StaleObjectException;
 
 class ProductCard implements IProductCard
 {
+    private $id;
 
+    public function __construct(IField $id)
+    {
+        $this->id = $id;
+    }
+
+    public function changeDescriptions(): IProductCard
+    {
+
+    }
+
+    public function changeTitle(): IProductCard
+    {
+
+    }
+
+    public function printYourSelf(): array
+    {
+
+    }
+
+    /**
+     * @throws StaleObjectException
+     */
+    public function moveToTrash(): void
+    {
+        $this->record()->delete();
+    }
+
+    private function record(): TableProductCards
+    {
+        return new TableProductCards([
+            'id' => $this->id->value(),
+            'isNewRecord' => false
+        ]);
+    }
 }
