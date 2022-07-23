d43721d55eef296d68f6b0c630484b67bf8f2f08
vetaly74 <cheltend201@yandex.ru>
2022-07-23 23:55:22 +0500

    Исправил проблему 1.создание формы в коллекции (не устраивает логика поиска ключа, откровенно слабая)

--- backend/models/collections/CollectionByForm.php
+++ backend/models/collections/CollectionByForm.php
@@ -14,31 +14,32 @@ class CollectionByForm implements ICollection
 {
     private $exampleOfCreate;
     private $form;
-    private $objectsType;
+    private $objectsPathInForm;
 
     /**
      * @param callable $exampleOfCreate - пример создания объекта на фход в функцию будет передаваться
      * элемент массива $needleList
+     * @param string $objectsPathInForm - 'foo.bar.objectType', will be used Array::helper
      * @param IForm $form
-     * @param string $objectsType - 'foo.bar.name'
      */
-    public function __construct(callable $exampleOfCreate, string $objectsType, IForm $form)
+    public function __construct(callable $exampleOfCreate, string $objectsPathInForm, IForm $form)
     {
         $this->form = $form;
         $this->exampleOfCreate = $exampleOfCreate;
-        $this->objectsType = $objectsType;
+        $this->objectsPathInForm = $objectsPathInForm;
     }
 
     /**
      * @return mixed - вернет список объектов, которые получились в следствии преобразования.
      * @throws NotValidatedFields
+     * @throws \Exception
      */
     public function list()
     {
         $validatedFields = $this->form->validatedFields();
-        $needleList = $this->searchKey($this->objectsType, $validatedFields);
+        $objectsArrayList = ArrayHelper::getValue($validatedFields, $this->objectsPathInForm, []);
         $objectList = [];
-        foreach ($needleList as $item){
+        foreach ($objectsArrayList as $item){
             $objectList[] = call_user_func($this->exampleOfCreate, $item);
         }
         return $objectList;
@@ -47,6 +48,7 @@ class CollectionByForm implements ICollection
     /**
      * @param IMedia $media - источник информации куда необходимо записать себя
      * @return IMedia - источник информации с только что записанными данными
+     * @throws NotValidatedFields
      */
     public function printTo(IMedia $media): IMedia
     {
@@ -55,28 +57,7 @@ class CollectionByForm implements ICollection
             /**@var IPrinter $object*/
             $medias[] = $object->printTo(new JsonMedia())->toArray();
         }
-
-        return $media->add($this->objectsType, $medias);
-    }
-
-    /**
-     * @param string $searchKey Ключ который ищим
-     * @param array $arr Массив в котором ищем
-     * @return mixed
-     */
-    private function searchKey(string $searchKey, array $arr)
-    {
-        // Если в массиве есть элемент с ключем $searchKey, то ложим в результат
-        if (isset($arr[$searchKey])) {
-            return $arr[$searchKey];
-        }
-        // Обходим все элементы массива в цикле
-        foreach ($arr as $key => $param) {
-            // Если эллемент массива есть массив, то вызываем рекурсивно эту функцию
-            if (is_array($param)) {
-                $this->searchKey($searchKey, $param);
-            }
-        }
-
+        $objectsType = end(explode('.', $this->objectsPathInForm));
+        return $media->add($objectsType, $medias);
     }
 }
