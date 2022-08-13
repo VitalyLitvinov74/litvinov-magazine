e3e83553dcef9173d5f8c9eb1d053c3a47bbf5f0
vetaly74 <cheltend201@yandex.ru>
2022-08-13 23:00:52 +0500

    как вариант стоит попробовать решить проблему через декоратор

---
+++ backend/models/forms/decorators/MergedForm.php
@@ -0,0 +1,57 @@
+<?php
+
+
+namespace app\models\forms\decorators;
+
+
+use app\models\forms\ProductForm;
+use vloop\entities\contracts\IForm;
+use vloop\entities\exceptions\NotValidatedFields;
+use vloop\entities\yii2\AbstractForm;
+use vloop\Yii2\Validators\ArrayValidator;
+use vloop\Yii2\Validators\CustomEachValidator;
+use vloop\Yii2\Validators\ValidationModel;
+use yii\base\Model;
+
+class MergedForm implements IForm
+{
+    private $origin;
+    private $needleMerge;
+    private $attributeName;
+    private $dynamicForm;
+
+    public function __construct(AbstractForm $origin, AbstractForm $needleMerge, string $attributeName)
+    {
+        $this->origin = $origin;
+        $this->needleMerge = $needleMerge;
+        $this->attributeName = $attributeName;
+        $this->dynamicForm = new ValidationModel();
+    }
+
+    public function validatedFields(): array
+    {
+        $dynamicForms =
+    }
+
+    private function totalForm(): Model
+    {
+        $rules = $this->origin->rules();
+        $attributes = $this->origin->getAttributes();
+        $rules = array_merge(
+            $rules,
+            [
+                ArrayValidator::class,
+                'subRules' => $this->needleMerge->rules()
+            ]
+        );
+        $attributes = array_merge(
+            $attributes,
+            [
+                $this->attributeName=>$this->needleMerge->getAttributes()
+            ]
+        );
+        $this->dynamicForm->attributes = $attributes;
+        $this->dynamicForm->setRules($rules);
+        return $this->dynamicForm;
+    }
+}
