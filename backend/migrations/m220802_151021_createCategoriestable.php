8c4474cf6c2c2f129d681166e3d8ca7c4389d68d
vetaly74 <cheltend201@yandex.ru>
2022-10-07 21:43:07 +0500

    профиксил миграцию

--- backend/migrations/m220802_151021_createCategoriestable.php
+++ backend/migrations/m220802_151021_createCategoriestable.php
@@ -17,30 +17,6 @@ class m220802_151021_createCategoriestable extends Migration
             'id' => $this->primaryKey(),
             'name' => $this->string()
         ]);
-
-        $this->createTable('categories_tree', [
-            'parent_id' => $this->integer(),
-            'child_id' => $this->integer(),
-            'level' => $this->integer()
-        ]);
-        $this->addForeignKey(
-            "child",
-            'categories_tree',
-            'child_id',
-            'categories',
-            'id',
-            'CASCADE',
-            'CASCADE'
-            );
-        $this->addForeignKey(
-            "parent",
-            'categories_tree',
-            'parent_id',
-            'categories',
-            'id',
-            'CASCADE',
-            'CASCADE'
-        );
         $record = new TableCategories(['name'=>"Главная"]);
         $record->save();
     }
@@ -50,9 +26,6 @@ class m220802_151021_createCategoriestable extends Migration
      */
     public function safeDown()
     {
-        $this->dropForeignKey('child', 'categories_tree');
-        $this->dropForeignKey('parent', 'categories_tree');
         $this->dropTable('categories');
-        $this->dropTable('categories_tree');
     }
 }
