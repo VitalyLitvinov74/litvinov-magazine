44bfec70a86dc5ed103af181136bfdea77253a08
VitalyLitvinov74 <cheltend201@yandex.ru>
2022-12-30 00:10:19 +0500

    переименовал папку со статичным шаблоном
    доделал 2 задачи ошибки 404 и переключения типов товара

--- frontend/layouts/error.vue
+++ frontend/layouts/error.vue
@@ -1,14 +1,22 @@
 <template>
   <div>
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
-    <h1 v-else>An error occurred</h1>
-    <NuxtLink to="/">Home page</NuxtLink>
+    <component v-bind:is="templateError"></component>
   </div>
 </template>
-
 <script>
-  export default {
-    props: ['error'],
-    layout: 'error' // you can set a custom layout for the error page
+import miocaError from "../templates/miocaError.vue";
+
+export default {
+  components: {
+    miocaError
+  },
+  created() {
+    this.templateError = this.$store.getters['layout/name'] + "Error";
+  },
+  data: function () {
+    return {
+      templateError: null
+    }
   }
+}
 </script>
