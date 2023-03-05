5aa0fa0da3aeaf610e926caee15c9ce967169c32
VitalyLitvinov74 <cheltend201@yandex.ru>
2023-03-05 18:22:59 +0500

    Убрал saveRelationTrait

--- backend/tables/BaseTable.php
+++ backend/tables/BaseTable.php
@@ -1,60 +1,31 @@
 <?php
-
-
 namespace app\tables;
 
-
-use app\models\contracts\IMedia;
-use app\models\contracts\IPrinter;
-use app\models\media\ArrayMedia;
-use lhs\Yii2SaveRelationsBehavior\SaveRelationsBehavior;
-use lhs\Yii2SaveRelationsBehavior\SaveRelationsTrait;
-use vloop\entities\exceptions\NotSavedData;
 use yii\db\ActiveRecord;
 use yii\helpers\Inflector;
-use yii\helpers\VarDumper;
 
 abstract class BaseTable extends ActiveRecord
 {
-    use SaveRelationsTrait;
-
-    public function behaviors()
+    /**
+     * преобразует поля таблицы в camelCase
+     * @return array
+     */
+    public function fields(): array
     {
-        return
-            array_merge(
-                parent::behaviors(),
-                [
-                    'saveRelations' => [
-                        'class' => SaveRelationsBehavior::class,
-                        'relationKeyName' => SaveRelationsBehavior::RELATION_KEY_RELATION_NAME
-                    ]
-                ]
-            );
-    }
-
-    public function load($data, $formName = '')
-    {
-        $loaded = parent::load($data, $formName);
-        if ($loaded && $this->hasMethod('loadRelations')) {
-            $this->loadRelations($data);
+        $fields = parent::fields();
+        foreach ($fields as $key=>$name){
+            unset($fields[$key]);
+            $fields[Inflector::variablize($key)]=$name;
         }
-        return $loaded;
+        return $fields;
     }
 
-    public function fields()
-    {
-        $merged =  array_merge(
-            parent::fields(),
-            static::extraFields()
-        );
-        foreach ($merged as $key=>$name){
-            unset($merged[$key]);
-            $merged[Inflector::variablize($key)]=$name;
-        }
-        return $merged;
-    }
-
-    public function setAttributes($values, $safeOnly = false)
+    /**
+     * @param $values
+     * @param $safeOnly
+     * @return void - при загрузке полей преобразует их из camelCase в snake_case
+     */
+    public function setAttributes($values, $safeOnly = false): void
     {
         $valuesWithNewKeys = [];
         foreach ($values as $name => $value) {
@@ -62,10 +33,5 @@ abstract class BaseTable extends ActiveRecord
             $valuesWithNewKeys[$name] = $value;
         }
         parent::setAttributes($valuesWithNewKeys, $safeOnly);
-    }
-
-    public static function find()
-    {
-        return new BaseQuery(static::class);
     }
 }
